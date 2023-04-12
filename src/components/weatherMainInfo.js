6a323e4e367c006dc7fe7c48a03a5d1ba62eb9c4
DamianPerdigon <perdigonalvarezdamian@gmail.com>
2023-04-12 15:25:27 +0100

    first commit

---
+++ src/components/weatherMainInfo.js
@@ -0,0 +1,36 @@
+
+
+export function WeatherMainInfo({weather}) {
+  return (
+    <div>
+        <div>{weather?.location.name}</div>
+        <div>{weather?.location.country}</div>
+        <div>
+            <div>
+                <img src={`http:${weather?.current.condition.icon}`}
+                width= "130"
+                alt={weather?.current.condition.text}/>
+            </div>
+            <div>
+                <div>
+                    {weather?.current.condition.text}
+                </div>
+                <div>
+                    {weather?.current.temp_c}º
+                </div>
+            </div>
+        </div>
+        <iframe
+        title='Map'
+        src={`https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d15057.534307180755!2d${weather.location.lon}5!3d${weather.location.lat}5!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2smx!4v1651103744472!5m2!1sen!2smx`}
+        width="100%"
+        height="350"
+        style={{ border: 0 }}
+        allowfullscreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"
+      ></iframe>
+    </div>
+  )
+}
+
